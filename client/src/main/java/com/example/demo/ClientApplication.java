13b9a35ebb49be28c8163c2db3f01ae0684b1db9
slahsk <slahsk12@gmail.com>
2017-09-02 22:56:30 +0900

    config client test

--- client/src/main/java/com/example/demo/ClientApplication.java
+++ client/src/main/java/com/example/demo/ClientApplication.java
@@ -1,7 +1,9 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,14 +11,19 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
+@RefreshScope
 public class ClientApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClientApplication.class, args);
 	}
 	
-    @RequestMapping("/greeting")
+    @Value("${message:demo.author}")
+    private String message;
+
+    
+    @RequestMapping("/")
     public String greeting() {
-        return "Hello from EurekaClient!";
+        return "Hello from EurekaClient!" + message;
     }
 }
